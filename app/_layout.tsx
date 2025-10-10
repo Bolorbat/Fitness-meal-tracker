c98b969eca21659f74f9f242270fd0409822aa48
Bolorbat <bolorbat.gombosure9@yahoo.com>
2025-10-10 16:30:58 +0800

    sync userData if user login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,8 @@ import { Stack, useRouter, usePathname, useSegments } from "expo-router";
 import { useEffect, useState, useRef } from "react";
 import { useFonts } from "expo-font";
 import { isFirstLaunch } from "@/utils/isFirstLaunch";
-import { SyncAll, SyncService } from "@/database/supabase/sync";
+import { SyncAll, SyncService, SyncTable } from "@/database/supabase/sync";
+import { ActivityIndicator, View, Text } from "react-native";
 
 function RootLayoutNav() {
   const [fontsLoaded] = useFonts({
@@ -15,27 +16,50 @@ function RootLayoutNav() {
   });
 
   const { user, initializing } = useAuth();
-  const onboardingStatus = useOnboardingStatus(user?.uid);
+  const [syncCompleted, setSyncCompleted] = useState(false);
   const [firstLaunch, setFirstLaunch] = useState<boolean | null>(null);
   const router = useRouter();
   const path = usePathname();
 
   useEffect(() => {
-    const initialize = async () => {
+    const initializeDB = async () => {
       try {
+        // await DatabaseConnection.getInstance().resetDatabase();
+        await initDB(); 
+      } catch (err) {
+        console.log("Error init db : ", err);
+      }
+    };
+
+    initializeDB();
+
+    return () => {
+      DatabaseConnection.getInstance().close();
+    };
+  }, []);
+
+  useEffect(() => {
+    const syncUserData = async () => {
+      if (!user) {
+        setSyncCompleted(false);
+        return;
+      }
+
+      try {
+        setSyncCompleted(false);
         const db = await initDB();
-        // DatabaseConnection.getInstance().resetDatabase();
-
-        const syncService = await SyncAll(db);
-
+        await SyncAll(db, user?.uid ?? "");
+        setSyncCompleted(true);
         console.log("Sync initialized successfully");
       } catch (error) {
         console.error("Failed at initialization:", error);
       }
     };
 
-    initialize();
-  }, []);
+    syncUserData();
+  }, [user]);
+
+  const onboardingStatus = useOnboardingStatus(user?.uid, syncCompleted);
 
   useEffect(() => {
     (async () => {
@@ -51,13 +75,35 @@ function RootLayoutNav() {
       router.replace("/(auth)/register");
     } else if (!user) {
       router.replace("/(auth)/login");
+    } else if (!syncCompleted) {
+      console.log("Waiting sync");
+      return;
     } else if (onboardingStatus === "done") {
       router.replace("/(tabs)/onboarding");
+    } else if (onboardingStatus === "incomplete") {
+      router.replace("/(auth)/userOnboarding");
     }
-  }, [user, onboardingStatus, firstLaunch, initializing, fontsLoaded, router]);
+  }, [
+    user,
+    onboardingStatus,
+    firstLaunch,
+    initializing,
+    fontsLoaded,
+    syncCompleted,
+    router,
+  ]);
 
   if (initializing || firstLaunch === null || !fontsLoaded) {
     return null;
+  }
+
+  if (user && !syncCompleted) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+        <Text>Loading...</Text>
+      </View>
+    );
   }
 
   return (
